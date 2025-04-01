7cc95d7d5e0610032e5df13d32628e86b3eba682
Hjwoon <hjw030705@naver.com>
2025-04-01 16:26:58 +0900

    ✨ Ngrok 사용하여 외부에서 meadiasoup 접속 구현 성공
    추후 공유기 대여하여 Nginx로 바꿔야 할 수도 있음

--- mediasoup-sfu/webpack.config.js
+++ mediasoup-sfu/webpack.config.js
@@ -1,33 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',  // 모드를 'development'로 설정
-    entry: './public/script.js',  // 시작 파일
+    entry: './public/client.js',
     output: {
-        filename: 'bundle.js',  // 번들된 파일
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            'mediasoup-client': path.resolve(__dirname, 'node_modules/mediasoup-client/lib/index.js'), // 정확한 경로 설정
-        },
-    },
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        port: 3001,
-    },
+    mode: 'development',
 };
