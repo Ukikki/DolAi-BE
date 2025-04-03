2f48afd4a694f2aa0e91fc05c03c1a5d6f05dd59
Hjwoon <hjw030705@naver.com>
2025-04-03 15:21:10 +0900

    ✨ Mediasoup와 SpringBoot 연동 완료(POST /meetings API 구현 완료)
    TURN 서버, Mediasoup 실행 확인할 것

---
+++ backend/src/main/java/com/dolai/backend/meeting/model/enums/Status.java
@@ -0,0 +1,8 @@
+package com.dolai.backend.meeting.model.enums;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
+
---
+++ mediasoup-sfu/roomManager.js
@@ -0,0 +1,49 @@
+// roomManager.js
+import mediasoup from 'mediasoup'
+
+export const mediaCodecs = [
+    {
+        kind: 'audio',
+        mimeType: 'audio/opus',
+        clockRate: 48000,
+        channels: 2,
+    },
+    {
+        kind: 'video',
+        mimeType: 'video/VP8',
+        clockRate: 90000,
+        parameters: {
+            'x-google-start-bitrate': 1000,
+        },
+    },
+]
+
+export let worker = null
+export const rooms = {}
+
+export const createWorker = async () => {
+    worker = await mediasoup.createWorker({
+        rtcMinPort: 10000, // 넓은 범위로 설정
+        rtcMaxPort: 20000,
+    })
+    console.log(`worker pid ${worker.pid}`)
+
+    worker.on('died', error => {
+        // This implies something serious happened, so kill the application
+        console.error('mediasoup worker has died')
+        setTimeout(() => process.exit(1), 2000) // exit in 2 seconds
+    })
+
+    return worker
+}
+
+export const createRoomIfNotExists = async (roomId) => {
+    if (!rooms[roomId]) {
+        const router = await worker.createRouter({ mediaCodecs })
+        rooms[roomId] = {
+            router,
+            peers: [],
+        }
+        console.log(`✅ Room "${roomId}" created.`)
+    }
+}
