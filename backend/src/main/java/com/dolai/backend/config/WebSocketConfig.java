a69698acb1056ebd337503d08ea66d97b6374630
Hjwoon <hjw030705@naver.com>
2025-05-21 19:27:57 +0900

    🚧 WIP

--- backend/src/main/java/com/dolai/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/dolai/backend/config/WebSocketConfig.java
@@ -1,29 +1,29 @@
-package com.dolai.backend.config;
+    package com.dolai.backend.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+    import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+    import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+    import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Configuration
+    @EnableWebSocketMessageBroker
+    public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+            config.enableSimpleBroker("/topic");
+            config.setApplicationDestinationPrefixes("/app");
+        }
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            //STT
+            registry.addEndpoint("/ws-stt").setAllowedOriginPatterns("*").withSockJS();
+
+            // 알림용
+            registry.addEndpoint("/ws-notification").setAllowedOriginPatterns("*").withSockJS();
+
+            // 채팅
+            registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
+        }
     }
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //STT
-        registry.addEndpoint("/ws-stt").setAllowedOriginPatterns("*").withSockJS();
-
-        // 알림용
-        registry.addEndpoint("/ws-notification").setAllowedOriginPatterns("*").withSockJS();
-
-        // 채팅
-        registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
-    }
-}
