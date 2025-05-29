49a4c4b3286410f9e874c065d371a9c78ff6690b
zuzihe <zuzihe@hansung.ac.kr>
2025-05-29 13:21:56 +0900

    🛠️CalendarDto 수정

--- backend/src/main/java/com/dolai/backend/calendar/model/CalendarDto.java
+++ backend/src/main/java/com/dolai/backend/calendar/model/CalendarDto.java
@@ -1,6 +1,7 @@
 package com.dolai.backend.calendar.model;
 
 import com.dolai.backend.meeting.model.Meeting;
+import com.dolai.backend.meeting.model.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +20,7 @@ public class CalendarDto {
     private String startTime;   // ISO 8601 문자열로 변환
     private List<String> participants;
     private String inviteUrl;
+    private Status status;
 
     public static CalendarDto from(Meeting meeting) {
         return CalendarDto.builder()
@@ -29,6 +31,7 @@ public class CalendarDto {
                         .map(participant -> participant.getUser().getId()) // userId 가져오기
                         .collect(Collectors.toList()))
                 .inviteUrl(meeting.getInviteUrl())
+                .status(meeting.getStatus())
                 .build();
     }
 }
